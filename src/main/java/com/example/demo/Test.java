1fd72e706dc231edb8b9fbf0999907670480ec23
shilongtang <qweiop123>
2022-04-04 23:02:12 +0800

    init

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+/**
+ * @Author: 青菜
+ * @Date: 2022/4/2 3:00 下午
+ * @Description:
+ * @Version 1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        // 登录返回
+        // uId, token
+        String uId = "10118929";
+        String token = "aA1xcM7X5KKXq+shgmOpXSJyEaTZWWPSyYUBKYnkYek=";
+        // 钱包地址 0x2cc66a638b4a8bb511d60326cfa77e72a8fa6aec
+
+        // getProductListByAlbumId
+
+        // getProductDetail 获取商品详情
+
+
+        // 专辑号
+        String albumId = "";
+
+
+        // 支付渠道
+        Integer payChannel = 22;
+
+        // 支付金额
+        double price = 19.9;
+
+        // 编号
+        Integer gNum = 113;
+
+        Integer subType = null;
+        Integer type = 0;
+
+
+        //  getProductListByAlbumId
+        String gId = "101009353";
+
+
+        String url = "https://api-h5.ibox.art/nft-mall-web/v1/nft/order/create";
+
+
+    }
+
+
+}
